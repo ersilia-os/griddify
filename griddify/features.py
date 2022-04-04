0df212df26064551a79645316dc4f71425f3f985
miquelduranfrigola <miquelduranfrigola@gmail.com>
2022-04-04 17:14:52 +0200

    remove features

--- griddify/features.py
+++ griddify/features.py
@@ -1,7 +1,10 @@
 import numpy as np
+import numpy as np
 import pandas as pd
 from scipy.spatial.distance import squareform
 from scipy.spatial.distance import pdist
+# from tqdm import tqdm
+# from .utils import Mixed_KSG
 
 
 class FeatureDistances(object):
@@ -15,18 +18,21 @@ class FeatureDistances(object):
         D[np.isnan(D)] = np.nanmax(D)
         return pd.DataFrame(D, columns=list(data.columns))
 
-""""
-
+"""
 class FeatureMutualInformation(object):
     def __init__(self, max_n=100000):
         self.max_n = max_n
 
     def calculate(self, data):
+        data = pd.DataFrame(data)
         X = np.array(data)[:self.max_n]
-        M = pass
-        for i in range(X.shape[0]-1):
-            for j in range(i+1, X.shape[0]):
+        M = np.zeros((X.shape[1], X.shape[1]))
+        for i in tqdm(range(X.shape[1])):
+            for j in range(i, X.shape[1]):
                 x = X[:,i]
                 y = X[:,j]
-
+                mi = Mixed_KSG(x,y,k=5)
+                M[i,j]=mi
+                M[j,i]=mi
+        return pd.DataFrame(M, columns=list(data.columns))
 """
